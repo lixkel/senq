02da86ddf22416e93cce0957255ddd418a65eaa9
Marek Mojík <marekmojik@protonmail.com>
2023-08-02 20:16:21 +0200

    senq initialization

---
+++ src/senq/Scraper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Senq {
+
+    public class SenqConf {
+        public readonly string web;
+    }
+
+    public class Scraper {
+        public void scrape(SenqConf conf) {
+            
+        }
+    }
+}
