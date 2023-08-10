336f588f3819fb41c55699a0502372174ef56bfa
Marek Mojík <marekmojik@protonmail.com>
2023-08-10 22:59:54 +0200

    DataMiner module init

---
+++ src/senq/DataMiner.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Senq {
+
+    public class DataMiner {
+        static Regex linkRegex = new Regex("<a\\s+(?:[^>]*?\\s+)?href=([\"])(.*?)\\1");
+
+        public static List<string> FindAll(string input, Regex regex) {
+            var matches = regex.Matches(input);
+    
+            var matchList = new List<string>();
+            foreach (Match match in matches) {
+                matchList.Add(match.Value);
+            }
+
+            return matchList;
+        }
+
+        public static List<string> FindWithRegex(string input, string pattern) {
+            try {
+                Regex regex = new Regex(pattern);
+                return FindAll(input, regex);
+            }
+            catch (ArgumentException) {
+                Console.WriteLine($"Invalid pattern: {pattern}");
+                return "";
+            }
+        }
+
+        public static List<string> FindLinks(string input) {
+            return FindAll(input, linkRegex); 
+        }
+    }
+}
