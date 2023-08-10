68d91b44d9ca1739122cd12b38520caa16e65b45
Marek Mojík <marekmojik@protonmail.com>
2023-08-11 00:18:26 +0200

    Main and output function

--- src/senq/Scraper.cs
+++ src/senq/Scraper.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Numerics;
 
 namespace Senq {
 
-    public class SenqConf {
-        public readonly string web;
+    public struct SenqConf {
+        public readonly string webAddr { get; init; }
+        public readonly string target { get; init; }
+        public readonly Action<string, string> output { get; init; }
     }
 
     public class Scraper {
+        RequestManager rm = new RequestManager();
+
+        public static void Main(string[] args) {
+            SenqConf conf = new SenqConf{ webAddr = "example.com", target = "example", output = csvOut };
+            Scraper scraper = new Scraper();
+            scraper.scrape(conf);
+        }
+
         public void scrape(SenqConf conf) {
-            
+            scrapePage(conf);
+        }
+
+        private void scrapePage(SenqConf conf) {
+            string content = rm.GET(conf.webAddr);
+            List<string> links = DataMiner.FindWithRegex(content, conf.target);
+        }
+
+        public static void csvOut(string webAddress, string content) {
+            Console.WriteLine($"{webAddress},{content}");
         }
     }
 }
