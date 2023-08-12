2821e7bdb27bc66aab7d670b875e82f39b42bfb5
Marek Mojík <marekmojik@protonmail.com>
2023-08-12 13:37:29 +0200

    added internal config struct

--- src/senq/Scraper.cs
+++ src/senq/Scraper.cs
@@ -2,20 +2,39 @@
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
 
 namespace Senq {
 
     public struct SenqConf {
-        public readonly string webAddr { get; init; }
-        public readonly string target { get; init; }
-        public readonly Action<string, string> output { get; init; }
+        public string webAddr { get; init; }
+        public string target { get; init; }
+        public Action<string, string> output { get; init; }
+        public int maxDepth { get; init; }
     }
 
+    internal struct InternalSenqConf {
+        private SenqConf _originalConf;
+        public Regex regex { get; }
+        public int depth {get; set; } = 0;
+        public string webAddr { get; init; }
+        public Action<string, string> output => _originalConf.output;
+        public int maxDepth => _originalConf.maxDepth;
+
+        public InternalSenqConf(SenqConf originalConf) {
+            _originalConf = originalConf;
+            webAddr = _originalConf.webAddr;
+            regex = new Regex(originalConf.target);
+        }
+    }
+
+
     public class Scraper {
-        RequestManager rm = new RequestManager();
+        private RequestManager rm = new RequestManager();
+        private readonly ConcurrentBag<Task> allTasks = new ConcurrentBag<Task>();
 
         public static void Main(string[] args) {
-            SenqConf conf = new SenqConf{ webAddr = "example.com", target = "example", output = CSVOut };
+            SenqConf conf = new SenqConf{ webAddr = "example.com", target = "example", output = CSVOut, maxDepth = 0 };
 
             Scraper scraper = new Scraper();
             scraper.Scrape(conf);
@@ -24,14 +43,19 @@ namespace Senq {
         public void Scrape(SenqConf conf) {
             BlockingCollection<(string, string)> queue = new BlockingCollection<(string, string)>();
 
+            InternalSenqConf internalConf = new InternalSenqConf(conf);
+
             Task.Factory.StartNew(() => {
                 OutputHandler(queue, conf.output);
             }, TaskCreationOptions.LongRunning);
 
-            Task.Run(() => ScrapePage(conf, queue));
+            var task = Task.Run(() => ScrapePage(internalConf, queue));
+            allTasks.Add(task);
+
+            Task.WhenAll(allTasks);
         }
 
-        private void ScrapePage(SenqConf conf, BlockingCollection<(string, string)> queue) {
+        private void ScrapePage(InternalSenqConf conf, BlockingCollection<(string, string)> queue) {
             string webPage = rm.GET(conf.webAddr);
 
             HandleMatches(conf, queue, webPage);
@@ -41,21 +65,26 @@ namespace Senq {
             List<string> webAddress = DataMiner.FindLinks(webPage);
         }
 
-        private void HandleMatches(SenqConf conf, BlockingCollection<(string, string)> queue, string webPage) {
-            List<string> matches = DataMiner.FindWithRegex(webPage, conf.target);
+        private void HandleMatches(InternalSenqConf conf, BlockingCollection<(string, string)> queue, string webPage) {
+            List<string> matches = DataMiner.FindAll(webPage, conf.regex);
 
             foreach (string match in matches ) {
                 queue.Add((conf.webAddr, match));
             }
         }
 
-        private void HandleLinks(SenqConf conf, BlockingCollection<(string, string)> queue, string webPage) {
-            List<string> links = DataMiner.FindWithRegex(webPage, conf.target);
+        private void HandleLinks(InternalSenqConf conf, BlockingCollection<(string, string)> queue, string webPage) {
+            if (conf.depth >= conf.maxDepth) {
+                return;
+            }
+
+            List<string> links = DataMiner.FindLinks(webPage);
 
             foreach (string link in links) {
-                SenqConf newConf = conf with { webAddr = link };
+                InternalSenqConf newConf = conf with { webAddr = link };
 
-                Task.Run(() => ScrapePage(newConf, queue));
+                var task = Task.Run(() => ScrapePage(newConf, queue));
+                allTasks.Add(task);
             }
         }
 
