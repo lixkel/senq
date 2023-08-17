28f084660bfb39f988750d2033eb01940f62f292
Marek Mojík <marekmojik@protonmail.com>
2023-08-17 15:23:33 +0200

    output to db implemented

--- src/senq/Output.cs
+++ src/senq/Output.cs
@@ -1,14 +1,11 @@
 using System;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 
 
 namespace Senq {
 
     public static class Output {
-
-        public static void db(string webAddress, string content) {
-            return;
-        }
 
         public static void CSVOut(string webAddress, string content) {
             Console.WriteLine($"{webAddress},{content}");
@@ -31,6 +28,42 @@ namespace Senq {
                 writer?.Flush();
                 writer?.Close();
                 writer?.Dispose();
+            }
+        }
+
+        public class DatabaseWriter : IDisposable {
+            private SqlConnection connection;
+            private string connectionString;
+            private string tableName;
+
+            public DatabaseWriter(string conString, string newTableName = "NONE") {
+                connectionString = conString;
+                tableName = newTableName;
+
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string checkTableQuery = $"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = '{tableName}') " +
+                                        $"CREATE TABLE {tableName} (WebAddress NVARCHAR(500), Content NVARCHAR(MAX))";
+
+                using (SqlCommand cmd = new SqlCommand(checkTableQuery, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            public void Write(string webAddress, string content) {
+                string insertQuery = $"INSERT INTO {tableName} (WebAddress, Content) VALUES (@WebAddress, @Content)";
+                using (SqlCommand cmd = new SqlCommand(insertQuery, connection)) {
+                    cmd.Parameters.AddWithValue("@WebAddress", webAddress);
+                    cmd.Parameters.AddWithValue("@Content", content);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            public void Dispose() {
+                connection?.Close();
+                connection?.Dispose();
             }
         }
     }
