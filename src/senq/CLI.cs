8a6a3634c75b5996129c1c1bacd6d3a1f409ebe1
Marek Mojík <marekmojik@protonmail.com>
2023-08-23 19:53:50 +0200

    options for db output

--- src/senq/CLI.cs
+++ src/senq/CLI.cs
@@ -36,17 +36,20 @@ namespace Senq {
         [Option('a', "userAgent", HelpText = "User agent string to use for requests. The tool supports user agent rotation if a list is provided.")]
         public string? userAgent { get; set; } = null;
 
-        [Option('o', "filePath", Default = OutputType.csv, HelpText = "Choose the output type: CSV or Database.")]
+        [Option('o', "outputType", Default = OutputType.csv, HelpText = "Choose the output type: CSV or Database.")]
         public OutputType outputType { get; set; }
 
         [Option('f', "outputFile", HelpText = "Output file location.")]
         public string? outputFile { get; set; } = null;
 
+        [Option('d', "dbString", HelpText = "Database connection string.")]
+        public string? dbString { get; set; } = null;
+
         [Option('t', "threads", HelpText = "Number of threads to use for multithreaded scraping.")]
         public int? threadCount { get; set; } = null;
 
-        [Option('d', "maxDepth", HelpText = "Maximum depth of following links.")]
-        public int maxDepth { get; set; }
+        [Option('m', "maxDepth", HelpText = "Maximum depth of following links.")]
+        public int maxDepth { get; set; } = 1;
     }
 
     public static class CLI {
@@ -57,16 +60,25 @@ namespace Senq {
             var parserResult = parser.ParseArguments<CLISenqConf>(args);
 
             parserResult.WithParsed(conf => { 
-                Scraper scraper = new Scraper();
-                
-                scraper.Scrape(conf);
+                HandleSuccessfulParse(conf);
             })
             .WithNotParsed((errs) =>  {
                 HandleParseErrors(parserResult, errs);
             });
         }
 
-        public static void HandleParseErrors<T>(ParserResult<T> result, IEnumerable<Error> errs) {
+        private static void HandleSuccessfulParse(CLISenqConf conf) {
+            if (conf.outputType == OutputType.db && string.IsNullOrEmpty(conf.dbString)) {
+                Console.WriteLine("Error: If you specify the 'outputType' option as 'db' (database), you must also provide a database connection string using the 'dbString' option.");
+                return;
+            }
+
+            Scraper scraper = new Scraper();
+                
+            scraper.Scrape(conf);
+        }
+
+        private static void HandleParseErrors<T>(ParserResult<T> result, IEnumerable<Error> errs) {
 
             // Check if help or version was requested, this library treats help nad version flags as errors
             if (errs.IsVersion() || errs.IsHelp())
