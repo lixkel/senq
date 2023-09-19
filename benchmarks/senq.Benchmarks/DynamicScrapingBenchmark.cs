1f59d25d9b858d8b3e35cd36c39b6db6139e6e85
Marek Mojík <marekmojik@protonmail.com>
2023-09-19 23:48:02 +0200

    added benchmark for dynamic sites

---
+++ benchmarks/senq.Benchmarks/DynamicScrapingBenchmark.cs
@@ -0,0 +1,25 @@
+using BenchmarkDotNet.Attributes;
+using Senq;
+
+namespace Benchmarks {
+    public class DynamicBenchmarks {
+        [Benchmark]
+        public void DynamicBenchmark() {
+
+        string result = "";
+            
+        var conf = new SenqConf  {
+            webAddr = "http://localhost/",
+            targetRegex = @"(?<target>spyware)",
+            useHostAddress = true,
+            output = Output.CSVString.GetWriter(str => { result = str; }),
+            webHandlerFactory = () => new PeppeteerManager();
+            maxDepth = 1,
+            stayOnDomain = true,
+        };
+
+        Scraper scraper = new Scraper();
+        scraper.Scrape(conf).GetAwaiter().GetResult();
+        }
+    }
+}
